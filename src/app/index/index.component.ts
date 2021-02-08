9bb60f5a5e60758a9a2b37ecb22507e4b430fecb
Ana-Maria Melinte <anamaria_melinte2000@yahoo.com>
2021-02-08 22:14:24 +0100

    clean scss files

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import noUiSlider from "nouislider";
 
 @Component({
   selector: "app-index",
@@ -13,35 +12,15 @@ export class IndexComponent implements OnInit, OnDestroy {
   date = new Date();
   pagination = 3;
   pagination1 = 1;
-  constructor() {}
+    constructor() { }
+
   scrollToDownload(element: any) {
     element.scrollIntoView({ behavior: "smooth" });
   }
+
   ngOnInit() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.add("index-page");
-
-    var slider = document.getElementById("sliderRegular");
-
-    noUiSlider.create(slider, {
-      start: 40,
-      connect: false,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
-
-    var slider2 = document.getElementById("sliderDouble");
-
-    noUiSlider.create(slider2, {
-      start: [20, 60],
-      connect: true,
-      range: {
-        min: 0,
-        max: 100
-      }
-    });
   }
 
   ngOnDestroy() {
