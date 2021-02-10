462ecefe5e7c0ac7804de8fd0e26d73a687be72c
Ana-Maria Melinte <anamaria_melinte2000@yahoo.com>
2021-02-10 01:02:20 +0100

    add product responsive page

--- src/app/pages/products-page/products-page.component.ts
+++ src/app/pages/products-page/products-page.component.ts
@@ -7,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPageComponent implements OnInit {
 
-    digital_pta_image_src = '../assets/img/digital-pta-image-green.png'
-    orthosis_image_src = '../assets/img/orthosis-image-green.png'
-
-    product_details = false
+    orthosis_product = false;
 
     constructor() { }
 
@@ -19,8 +16,6 @@ export class ProductsPageComponent implements OnInit {
     }
 
     change_product() {
-        this.product_details = !this.product_details 
-        this.digital_pta_image_src = '../assets/img/digital-pta-image-green.png'
-        this.orthosis_image_src = '../assets/img/orthosis-image-green.png'
+        this.orthosis_product = !this.orthosis_product 
     }
 }
