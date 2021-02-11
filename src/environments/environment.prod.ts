9f362e5ca32f0dd2728d6076519558888467749c
Ana-Maria Melinte <anamaria_melinte2000@yahoo.com>
2021-02-12 00:33:26 +0100

    add firebase detaisl to environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDN19lcCQ6XuvE2rGu3PBAs_vhZQpsbsY0",
+    authDomain: "exigo-38b75.firebaseapp.com",
+    databaseURL: "https://exigo-38b75-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "exigo-38b75",
+    storageBucket: "exigo-38b75.appspot.com",
+    messagingSenderId: "33769071382",
+    appId: "1:33769071382:web:12463526a99bfc0a5b6637",
+    measurementId: "G-Q3PFDJRMZ2"
+  }
 };
